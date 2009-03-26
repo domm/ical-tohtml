9ca09be97e635c6af8b681469c159f1d96f35fca
Thomas Klausner <domm@cpan.org>
2009-03-26 21:02:14 +0100

    initial commit

---
+++ lib/ICal/ToHTML.pm
@@ -0,0 +1,34 @@
+package ICal::ToHTML;
+
+use strict;
+use warnings;
+use 5.010;
+use version; our $VERSION = version->new('0.01');
+
+1;
+__END__
+
+=head1 NAME
+
+ICal::ToHTML -
+
+=head1 SYNOPSIS
+
+  use ICal::ToHTML;
+
+=head1 DESCRIPTION
+
+ICal::ToHTML is
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
